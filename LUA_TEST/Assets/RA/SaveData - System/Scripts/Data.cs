f8c621441b750afd327ec06b5ccdc15558512a29
pokelocos <nare.rare.fire@gmail.com>
2022-06-16 23:15:37 -0400

    Add SaveData-Sys
    * La info a guardar se encuentra comentada, recordar descomentarla para que funcone

---
+++ LUA_TEST/Assets/RA/SaveData - System/Scripts/Data.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DataSystem
+{
+    /// <summary>
+    /// This is a generic class that is used by the rest of the solution to handle the data.
+    /// Use this class as a base object to store all the information that you don't want to
+    /// be lost from your program. Remember that all objects must be "Serializable" so that
+    /// their value is saved correctly.
+    /// </summary>
+    [System.Serializable]
+    public class Data
+    {
+        // <----
+        // Fill, This area whit varibles and object you want to save.
+
+        /* //DESCOMENTAR
+        public GeneralStatistics stats = new GeneralStatistics();
+        public GameState GameState;
+        public OptionData options = new OptionData();
+        public WorkShopTraker WS_Traker = new WorkShopTraker(); 
+        */
+
+        // <----
+
+        /// <summary>
+        /// Basic constructor, necesary to make a data file.
+        /// </summary>
+        public Data(){ }
+
+        public bool IsGameInProgress()
+        {
+            return false;//GameState != null; //DESCOMENTAR
+        }
+
+
+    }
+
+
+}
