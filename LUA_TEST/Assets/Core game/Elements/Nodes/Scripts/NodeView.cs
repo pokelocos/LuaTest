eda02d38a13e4b1b4da6a6f8d1919678a8c277dd
pokelocos <nare.rare.fire@gmail.com>
2022-06-19 16:41:02 -0400

    Update NodeView.cs

--- LUA_TEST/Assets/Core game/Elements/Nodes/Scripts/NodeView.cs
+++ LUA_TEST/Assets/Core game/Elements/Nodes/Scripts/NodeView.cs
@@ -6,13 +6,21 @@ using UnityEngine;
 public class NodeView : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer body;
-    [SerializeField] private SpriteRenderer nodeIcon;
+    [SerializeField] private SpriteRenderer icon;
     [SerializeField] private SpriteRenderer fillBar;
+
+    private Vector3 _defaultIconScaleRatio;
+
+    public void Awake()
+    {
+        _defaultIconScaleRatio = icon.sprite.rect.size * icon.transform.localScale;
+    }
 
     public void SetView(NodeData nodeData)
     {
         body.color = nodeData.bgColor;
-        nodeIcon.sprite = nodeData.icon;
+        icon.sprite = nodeData.icon;
+        icon.transform.localScale = icon.sprite.rect.size / _defaultIconScaleRatio;
     }
 
     public void SetBodyColor(Color color)
