4a57b24aa6937448e30df57a5e9f96755986c8cd
pokelocos <nare.rare.fire@gmail.com>
2022-06-20 19:08:30 -0400

    Update InputHandler.cs

--- LUA_TEST/Assets/Core game/Handlers/InputHandler.cs
+++ LUA_TEST/Assets/Core game/Handlers/InputHandler.cs
@@ -1,25 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using UnityEngine.EventSystems;
+using System;
 
-public class InputHandler : MonoBehaviour
+namespace RA.Inputs
 {
-    private const float DOUBLE_CLICK_TIME = .2f;
-
-    // Start is called before the first frame update
-    void Start()
+    public static class InputHandler
     {
-        
+        private static float DOUBLE_CLICK_TIME = 0.15f;
+
+        private static Dictionary<int, float> lastInputs = new Dictionary<int, float>();
+
+        public static bool MouseDoubleCLick(int input)
+        {
+            if (UnityEngine.Input.GetMouseButtonDown(input))
+            {
+                
+                float last;
+                if (lastInputs.TryGetValue(input, out last))
+                {
+                    Debug.Log("B");
+                    lastInputs.Add(input,Time.unscaledTime);
+                    return false;
+                }
+                Debug.Log("A: " + last);
+
+                float delta = Time.unscaledTime - last;
+                Debug.Log("C: "+ delta);
+
+                if (delta <= DOUBLE_CLICK_TIME)
+                {
+                    Debug.Log("D");
+                    lastInputs.Add(input, Time.unscaledTime);
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
-
-public interface ISelectableObject
-{
 
 }
