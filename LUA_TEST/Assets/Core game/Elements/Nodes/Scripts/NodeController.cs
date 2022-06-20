ede4088395fc8a5547397dc276c0a361977ef643
pokelocos <nare.rare.fire@gmail.com>
2022-06-19 23:23:18 -0400

    Update NodeController.cs
    add CurrentRecipe property
    Add start fucntion
    add slect current recipe
    add null conditon on try start production
    add inventory events

--- LUA_TEST/Assets/Core game/Elements/Nodes/Scripts/NodeController.cs
+++ LUA_TEST/Assets/Core game/Elements/Nodes/Scripts/NodeController.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+// un nodo no puede tenr mas de una recipe con el mismo input
+
 [RequireComponent(typeof(NodeView))]
 [RequireComponent(typeof(ClockTimer))]
 public class NodeController : MonoBehaviour //, ISelectableObject
@@ -17,19 +19,38 @@ public class NodeController : MonoBehaviour //, ISelectableObject
     private List<ConnectionController> input = new List<ConnectionController>();
     private List<ConnectionController> output = new List<ConnectionController>();
     private Inventory inventory = new Inventory();
-    private RecipeData current;
+    private RecipeData currentRecipe; // << esta varible puede ser problematica por posible NULL
 
     public delegate void ConnectionEvent(ConnectionController connection, NodeController node);
     public event ConnectionEvent OnConnect;
     public event ConnectionEvent OnDisconnect;
     public event ConnectionEvent OnReceiveProduct;
 
+    public RecipeData CurrentRecipe
+    {
+        get => currentRecipe;
+        private set
+        {
+            if(value != null)
+            {
+                currentRecipe = value;
+                timer.Max = value.time;
+            }
+        }
+    }
+
     private void Awake()
     {
         nodeView = GetComponent<NodeView>();
         timer = GetComponent<ClockTimer>();
+    }
 
+    private void Start()
+    {
         this.InitEvents();
+
+        SelectCurrentRecipe();
+        TryStartProduction();
     }
 
     public void Init(NodeData data, float startTime)
@@ -43,27 +64,65 @@ public class NodeController : MonoBehaviour //, ISelectableObject
     {
         input.Remove(connection); // esto podria estar con un if els y ais sacar eventos UwU
         output.Remove(connection);
+
+        OnDisconnect?.Invoke(connection,this);
     }
 
     private void InitEvents()
     {
         // Node Events
-        //timer.OnStart += (n) => { };
-        timer.OnEnd += (n) => { TryStartProduction(); };
+        //timer.OnStart += (clock) => { };
+        timer.OnEnd += (clock) => { TryStartProduction(); };
+        timer.OnUpdate += (clock) => { nodeView.SetBarAmount((clock.Current / currentRecipe.time)); };
 
         // Connection Event
         OnReceiveProduct += (c, n) => { TryStartProduction(); };
+        OnConnect += (c, n) => { SelectCurrentRecipe(); };
+        OnDisconnect += (c, n) => { SelectCurrentRecipe(); };
+
+        // Inventory event
+        //inventory.OnSuccessReceived += (c) => { };
+        //inventory.OnFailReceived += (c) => { c. }; // set red connection;
+    }
+
+    /// <summary>
+    /// Revisa las recipes que tiene el nodo y se queda con la que pueda hacer dependiendo delos inputsa que tenga
+    /// </summary>
+    /// <returns></returns>
+    private void SelectCurrentRecipe()
+    {
+        data.recipes.OrderByDescending(r => r.inputIngredients.Count());
+        currentRecipe = null;
+
+        var ings = input.Select(x => x.GetIngredientAllowed());
+        var recipes = data.recipes;
+        for (int i = 0; i < recipes.Count(); i++)
+        {
+            if (recipes[i].inputIngredients.Count() == 0)
+            {
+                CurrentRecipe = recipes[i];
+                break;
+            }
+
+            bool hasMatch = input.Select(x => x.GetIngredientAllowed())
+                          .Intersect(ings).Any();
+            if (hasMatch)
+            {
+                CurrentRecipe = recipes[i];
+                break;
+            }
+        }
     }
 
     private bool TryStartProduction()
     {
         if (timer.IsActive()) // ya esta producionedo
             return false;
-
-        var ingredients = current.inputIngredients;
+        if (currentRecipe == null)
+            return false;
+        var ingredients = currentRecipe.inputIngredients;
         if (!inventory.HaveIngredients(ingredients)) // no tengo los ingredientes
             return false;
-
         inventory.RemoveIngedients(ingredients);
         timer.StartClock();
         return true;
@@ -72,6 +131,10 @@ public class NodeController : MonoBehaviour //, ISelectableObject
     private class Inventory
     {
         private List<Report> receivedIngredient = new List<Report>();
+
+        public delegate void InventoryEvent(ConnectionController connection);
+        public event InventoryEvent OnSuccessReceived;
+        public event InventoryEvent OnFailReceived;
 
         protected void TryAddIngredient(ConnectionController from, IngredientData ingredient) // change name to "TryAddReport" (??)
         {
@@ -82,10 +145,11 @@ public class NodeController : MonoBehaviour //, ISelectableObject
             if(reports.Count <= 0) 
             {
                 receivedIngredient.Add(new Report(from, ingredient));
+                OnSuccessReceived?.Invoke(from);
             }
             else
             {
-                // ya exite asi que no hago nada UwU // Evento (???)
+                OnFailReceived?.Invoke(from);
             }
         }
 
