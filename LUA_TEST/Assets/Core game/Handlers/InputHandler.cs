49384ce505fda731496499b19e90b9f9cd15c283
pokelocos <nare.rare.fire@gmail.com>
2022-06-19 23:23:56 -0400

    add input handler empty

---
+++ LUA_TEST/Assets/Core game/Handlers/InputHandler.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    private const float DOUBLE_CLICK_TIME = .2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public interface ISelectableObject
+{
+
+}
