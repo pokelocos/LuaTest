61f62d28763b25cab94a371b1205dec1d4b46c97
pokelocos <nare.rare.fire@gmail.com>
2022-08-16 20:42:13 -0400

    a

--- MicroFactory-Repo/Assets/Resources/Mods/test_mod/main.lua
+++ MicroFactory-Repo/Assets/Resources/Mods/test_mod/main.lua
@@ -4,3 +4,40 @@ local OnStartGame = function ()
 end
 
 return OnStartGame
+
+
+
+local OnConnect = function ()
+    MF.AddMoney(-50);
+    MF.SpanwParticle("substract_money",50)
+end
+
+return OnConnect
+
+
+
+local OnDisconnect = function ()
+    MF.AddMoney(-50);
+    MF.SpanwParticle("substract_money",50)
+end
+
+return OnDisconnect
+
+
+
+local OnSelectNode = function ()
+    MF.AddMoney(-25);
+    MF.SpanwParticle("substract_money",25)
+end
+
+return OnSelectNode
+
+
+
+
+local OnChangeTimeSpeed = function ()
+    MF.AddMoney(-100);
+    MF.SpanwParticle("substract_money",100)
+end
+
+return OnChangeTimeSpeed
